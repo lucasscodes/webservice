f8e383edf4844c1cd6e7be538fe0e4ea277088d2
Lucas S <itlucasschmidt@outlook.de>
2024-04-28 22:59:50 +0200

    .

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ module.exports = function () {
         host: 'localhost',
         port: '3306',
         user: 'root',
-        password: 'test123',
+        password: '',
         database: 'cache'
       });
 };
